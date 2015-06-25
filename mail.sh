2d0cfb598f1d458bdc4ab3f360f9e9a071f88a8b
bryan kennedy <bkennedy@smm.org>
2015-06-25 17:25:25 -0500

    Adding arguments for banned & unbanned

--- mail.sh
+++ mail.sh
@@ -6,10 +6,21 @@ source api.conf
 # Send mail message via Mailgun
 function send_sms {
   echo "Sending"
-  mailgun_call="/usr/bin/curl -s --user 'api:$mailgun_api' https://api.mailgun.net/v3/$mailgun_domain/messages -F from='$from' -F to='$to' -F subject='$server - fail2ban blocked an IP' -F text='fail2ban blocked IP: <ip>'"
+  mailgun_call="/usr/bin/curl -s --user 'api:$mailgun_api'
+  https://api.mailgun.net/v3/$mailgun_domain/messages -F from='$from' \
+  -F to='$to' \
+  -F subject='$server - fail2ban $message an IP' \
+  -F text='fail2ban $1 IP: <ip>'"
   echo $mailgun_call
   eval $mailgun_call
   exit
 }
 
-send_sms
+if [ "$1" = 'ban' ]
+then
+  message="banned"
+  send_sms $message
+else
+  message="unbanned"
+  send_sms $message
+fi
