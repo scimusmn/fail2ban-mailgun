4849e2757f05045e4a5247e8e8187e0c1775ac6f
bryan kennedy <bkennedy@smm.org>
2015-06-25 15:07:56 -0500

    Initial script to send mail via mailgun curl

---
+++ mail.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Get secrets
+source api.conf
+
+# Send mail message via Mailgun
+function send_sms {
+  echo "Sending"
+  mailgun_call="/usr/bin/curl -s --user 'api:$mailgun_api' https://api.mailgun.net/v3/$mailgun_domain/messages -F from='$from' -F to='$to' -F subject='$server - fail2ban blocked an IP' -F text='fail2ban blocked IP: <ip>'"
+  echo $mailgun_call
+  eval $mailgun_call
+  exit
+}
+
+send_sms
